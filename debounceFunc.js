8e4a992635ece1f8616111f1199a46299962a8b7
Konrad-Jalowy <sitesystem@wp.pl>
2024-07-29 12:06:30 +0200

    change

--- debounceFunc.js
+++ debounceFunc.js
@@ -1,22 +1,15 @@
-function debounceFunc(func, timeout = 3000){
-
-    let timer = null;
-    return (...args) => {
-
-      if (timer === null) {
-        
-        func(...args);
-
-        timer = setTimeout(() => { 
-            timer = null
-        }, timeout);
-
-      } else {
-        timer = setTimeout(() => { 
-            timer = null
-            func(...args); 
-        }, timeout);
-      }
-     
+const debounce = (mainFunction, delay) => {
+   
+    let timer;
+  
+   
+    return function (...args) {
+      
+      clearTimeout(timer);
+  
+      
+      timer = setTimeout(() => {
+        mainFunction(...args);
+      }, delay);
     };
-  }
+  };
