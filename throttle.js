40f19b2186953295951015119d952412cfab7fb8
Konrad-Jalowy <sitesystem@wp.pl>
2024-07-29 11:39:38 +0200

    throttle

---
+++ throttle.js
@@ -0,0 +1,17 @@
+function throttle(mainFunction, delay) {
+
+    let timerFlag = null; 
+  
+    return (...args) => {
+      
+    if (timerFlag === null) { 
+
+        mainFunction(...args); 
+
+        timerFlag = setTimeout(() => { 
+          timerFlag = null; 
+        }, delay);      
+      
+       }
+    };
+  }
